bc828b55e98899e21895fac5ffe82fffc32395ff
DESKTOP-NG333GB\earth <patchara.pck@gmail.com>
2022-10-02 00:54:02 +0700

    [Add] Basic health check

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHealthChecks();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<IPassWordService,PassWordService>();
 
@@ -22,6 +23,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.MapHealthChecks("/healthCheck");
 
 app.MapControllers();
 
