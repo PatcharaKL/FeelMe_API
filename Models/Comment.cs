a432c97e3a663d59ce4991427166d3bac9dedc29
Sayfar <620107030028@dpu.ac.th>
2022-10-30 18:16:39 +0700

    ConnectDbFeelMe

---
+++ Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FeelMe.Models
+{
+    public partial class Comment
+    {
+        public int CommentId { get; set; }
+        public string CommentText { get; set; } = null!;
+        public DateTime Created { get; set; }
+        public DateTime? EditTime { get; set; }
+        public int AccountId { get; set; }
+        public int BoradId { get; set; }
+    }
+}
