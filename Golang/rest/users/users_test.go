d71d9fc5f94f483228af748f60b89b8ef84fed23
sayfar <sayfareiei@gmail.com>
2023-01-30 21:07:17 +0700

    Add userlogout

---
+++ Golang/rest/users/users_test.go
@@ -0,0 +1,185 @@
+package users
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserLogin(t *testing.T) {
+	tests := []struct {
+		name         string
+		body         *bytes.Buffer
+		expectedCode int
+	}{
+		// {
+		// 	name: "testSucceed",
+		// 	body: bytes.NewBufferString(`{
+		// 		"email": "user1",
+		// 		"password": "123"
+		// 		}`),
+		// 	expectedCode: http.StatusOK,
+		// },
+		{
+			name: "testUnauthorized",
+			body: bytes.NewBufferString(`{
+				"email": "user1",
+				"password": "1234"
+			}`),
+			expectedCode: http.StatusUnauthorized,
+		},
+		{
+			name: "testInternalServerError",
+			body: bytes.NewBufferString(`{
+				"emails": "user1",
+				"password": "124"
+			}`),
+			expectedCode: http.StatusInternalServerError,
+		},
+		{
+			name: "testBadRequest",
+			body: bytes.NewBufferString(`{
+				"emails": "user1",
+				: "124"
+			}`),
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "testExecError",
+			body: bytes.NewBufferString(`{
+				"email": "user1",
+				"password": "123"
+				}`),
+			expectedCode: http.StatusInternalServerError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rec, c := setupTestServer(http.MethodPost, "/userlogin", tt.body)
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer db.Close()
+			sqlmock.NewRows([]string{"refreshToken", "account_id", "exp", "isValid"}).
+				AddRow("d840e7da-bd5b-490a-8f2a-83648c114d6c-f0a82297-0ec7-4d74-baf2-1db6b4a03c14-43a2d592-09e3-4520-abf3-531ee2b64ea0-7dde1858-e8e5-4dbb-9c28-963117f4dec9",
+					1, "2023-02-04 07:52:09", true)
+			var tokenRows *sqlmock.Rows
+			accountRow := sqlmock.NewRows([]string{"account_id", "email", "password_hash", "name",
+				"surname", "avatar_url", "apply_date", "is_active", "hp", "level", "created", "department_id", "posirion_id", "company_id"}).
+				AddRow(1, "user1", "$2a$11$nn97NWhYg8ALx5FclSJlVOSIhrNL4eR3hyX7exa/ZRVmsEnPDIkkm",
+					"Patchara", "Kleebbua", "", "2019-01-01 00:00:00", true, 100, 1, "2019-01-01 00:00:00", 1, 1, 1)
+			tokenRows = sqlmock.NewRows([]string{"refreshToken"}).AddRow("1234568632").AddRow("1234587464")
+			if tt.name != "testInternalServerError" {
+				mock.ExpectQuery("SELECT \\* FROM accounts WHERE email=\\?").WithArgs("user1").WillReturnRows(accountRow)
+				mock.ExpectQuery("SELECT (.+) FROM refresh_token WHERE (.+)").WithArgs(1, true).WillReturnRows(tokenRows)
+				expectPrepare := mock.ExpectPrepare("UPDATE refresh_token SET (.+) WHERE (.+)")
+				if tt.name != "testExecError" {
+					expectPrepare.ExpectExec().WithArgs(false, "d840e7da-bd5b-490a-8f2a-83648c114d6c-f0a82297-0ec7-4d74-baf2-1db6b4a03c14-43a2d592-09e3-4520-abf3-531ee2b64ea0-7dde1858-e8e5-4dbb-9c28-963117f4dec9").WillReturnResult(sqlmock.NewResult(0, 0))
+				}
+
+				// expectPrepare.ExpectExec().WithArgs(false, "d840e7da-bd5b-490a-8f2a-83648c114d6c-f0a82297-0ec7-4d74-baf2-1db6b4a03c14-43a2d592-09e3-4520-abf3-531ee2b64ea0-7dde1858-e8e5-4dbb-9c28-963117f4dec9").WillReturnResult(sqlmock.NewResult(0, 0))
+			}
+			h := Handler{db}
+			err = h.UserLoginHandler(c)
+			if assert.NoError(t, err) {
+				assert.Equal(t, tt.expectedCode, rec.Code)
+			}
+		})
+	}
+}
+
+func TestUserLogOut(t *testing.T) {
+	tests := []struct {
+		name         string
+		body         *bytes.Buffer
+		expectedCode int
+	}{
+		{
+			name: "testSucceed",
+			body: bytes.NewBufferString(`{
+				"refreshToken":"d840e7da-bd5b-490a-8f2a-83648c114d6c-f0a82297-0ec7-4d74-baf2-1db6b4a03c14-43a2d592-09e3-4520-abf3-531ee2b64ea0-7dde1858-e8e5-4dbb-9c28-963117f4dec9"
+				}`),
+			expectedCode: http.StatusOK,
+		},
+		{
+			name: "testBadRequest",
+			body: bytes.NewBufferString(`{
+				"refreshTokensssss":"4dbb-9c28-963117f4dec9",12313
+				}`),
+			expectedCode: http.StatusBadRequest,
+		},
+		{
+			name: "testInternalServerError",
+			body: bytes.NewBufferString(`{
+				"refreshToken":"d840e7da-bd5b-490a-8f2a-83648c114d6c"
+				}`),
+			expectedCode: http.StatusInternalServerError,
+		},
+
+		{
+			name: "testExecError",
+			body: bytes.NewBufferString(`{
+				"refreshToken":"d840e7da-bd5b-490a-8f2a-83648c114d6c-f0a82297-0ec7-4d74-baf2-1db6b4a03c14-43a2d592-09e3-4520-abf3-531ee2b64ea0-7dde1858-e8e5-4dbb-9c28-963117f4dec9"
+				}`),
+			expectedCode: http.StatusInternalServerError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rec, c := setupTestServer(http.MethodPost, "/users/userlogout", tt.body)
+
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer db.Close()
+
+			sqlmock.NewRows([]string{"refreshToken", "account_id", "exp", "isValid"}).
+				AddRow("d840e7da-bd5b-490a-8f2a-83648c114d6c-f0a82297-0ec7-4d74-baf2-1db6b4a03c14-43a2d592-09e3-4520-abf3-531ee2b64ea0-7dde1858-e8e5-4dbb-9c28-963117f4dec9",
+					1, "2023-02-04 07:52:09", true).
+				AddRow("d840e7da-bd5b-490a-8f2a-83648c114d6c-f0a82297-0ec7-4d74-baf2-1db6b4a03c14-43a2d592-09e3-4520-abf3-531ee2b64ea0-7dde1858-e8e5-4dbb-9c28-963117f4dec9",
+					1, "2023-02-04 07:52:09", true)
+			if tt.name != "testInternalServerError" {
+				expectPrepare := mock.ExpectPrepare("UPDATE refresh_token SET (.+) WHERE (.+)")
+				if tt.name != "testExecError" {
+					expectPrepare.ExpectExec().WithArgs(false, "d840e7da-bd5b-490a-8f2a-83648c114d6c-f0a82297-0ec7-4d74-baf2-1db6b4a03c14-43a2d592-09e3-4520-abf3-531ee2b64ea0-7dde1858-e8e5-4dbb-9c28-963117f4dec9").WillReturnResult(sqlmock.NewResult(0, 0))
+				}
+			}
+			h := Handler{db}
+
+			err = h.UserLogOutHandler(c)
+
+			if assert.NoError(t, err) {
+				assert.Equal(t, tt.expectedCode, rec.Code)
+			}
+		})
+	}
+}
+func setupTestServer(method, uri string, body *bytes.Buffer) (*httptest.ResponseRecorder, echo.Context) {
+	e := echo.New()
+	req := httptest.NewRequest(method, uri, body)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	return rec, c
+}
+func TestNewApplicationInit(t *testing.T) {
+	// Arrange
+	db, _, _ := sqlmock.New()
+	expected := "*users.Handler"
+
+	// Act
+	n := NewApplication(db)
+	actual := fmt.Sprintf("%T", n)
+
+	// Assert
+	assert.Equal(t, expected, actual)
+}
