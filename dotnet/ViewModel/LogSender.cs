109ebccc2ca947596331fecd3c565716381baf20
PatcharaKL <patchara.pck@gmail.com>
2023-01-30 19:55:41 +0700

    delete dotnet relate and sonarcloud workflow

--- dotnet/ViewModel/LogSender.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet.ViewModel
-{
-    public class LogSender
-    {
-        public sbyte type{get;set;}
-        public int amount{get;set;}
-    }
-}
--- dotnet/ViewModel/UserLogin.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet.ViewModel
-{
-    public class UserLogin
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
