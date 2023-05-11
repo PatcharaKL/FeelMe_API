fb33881e1ab3f057b1e1d905c615f2bf355d8ac2
DESKTOP-M2U6K5I\Sayfar <sayfareiei@gmail.com>
2023-05-11 22:55:47 +0700

    add api edit image profile

--- Golang/go.mod
+++ Golang/go.mod
@@ -3,10 +3,12 @@ module github.com/PatcharaKL/FeelMe_API
 go 1.19
 
 require (
+	github.com/Azure/azure-storage-blob-go v0.15.0
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/go-sql-driver/mysql v1.7.0
 	github.com/golang-jwt/jwt/v4 v4.4.3
 	github.com/google/uuid v1.3.0
+	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo-jwt/v4 v4.1.0
 	github.com/labstack/echo/v4 v4.10.0
 	github.com/labstack/gommon v0.4.0
@@ -16,7 +18,6 @@ require (
 
 require (
 	github.com/Azure/azure-pipeline-go v0.2.3 // indirect
-	github.com/Azure/azure-storage-blob-go v0.15.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- Golang/service/uploadfile.service.go
+++ Golang/service/uploadfile.service.go
@@ -5,20 +5,28 @@ import (
 	"fmt"
 	"mime/multipart"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/Azure/azure-storage-blob-go/azblob"
 	"github.com/google/uuid"
+	"github.com/joho/godotenv"
 )
 
-const (
-	// Create a new Blob Service Client using your Azure Storage account credentials
-	accountName   = "feelme"
-	accountKey    = "YInTKgO30iWulle6Q5GvUCBJnZG7A+H9MNHp22PmvaWZozjff9J3o86OT01+d9AezbqpIyC8Gw32+AStPonhyg=="
-	containerName = "feelme-image/profile"
-)
+// const (
+// 	// Create a new Blob Service Client using your Azure Storage account credentials
+// 	accountName   = "feelme"
+// 	accountKey    = "YInTKgO30iWulle6Q5GvUCBJnZG7A+H9MNHp22PmvaWZozjff9J3o86OT01+d9AezbqpIyC8Gw32+AStPonhyg=="
+// 	containerName = "feelme-image/profile"
+// )
 
 func UploadService(dirName, fileName string, file multipart.File) (string, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return err.Error(), err
+	}
+	accountName := os.Getenv("ACCOUNT_BOLB_NAME")
+	accountKey := os.Getenv("ACCOUNT_BOLB_KEY")
 	arr := strings.Split(fileName, ".")
 	newFileName := uuid.New().String() + "." + arr[len(arr)-1]
 
@@ -29,7 +37,7 @@ func UploadService(dirName, fileName string, file multipart.File) (string, error
 
 	p := azblob.NewPipeline(credential, azblob.PipelineOptions{})
 	u, _ := url.Parse(
-		fmt.Sprintf("https://%s.blob.core.windows.net/%s", accountName, containerName),
+		fmt.Sprintf("https://%s.blob.core.windows.net/%s", accountName, dirName),
 	)
 	containerURL := azblob.NewContainerURL(*u, p)
 
